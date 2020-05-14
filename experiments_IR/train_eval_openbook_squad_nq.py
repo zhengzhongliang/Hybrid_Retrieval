bbe7f67154f34555d9d9858fde362f7f1d01b09a
Zhengzhong Liang <zhengzhongliang@Zhengzhongs-MacBook-Pro.local>
2020-05-14 11:37:22 -0700

    debug

--- experiments_IR/train_eval_openbook_squad_nq.py
+++ experiments_IR/train_eval_openbook_squad_nq.py
@@ -82,7 +82,8 @@ class BertSQuADRetriever(nn.Module):
             tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
             print("="*20)
             for query_token_id in batch["query_token_ids"]:
-                print(tokenizer.convert_ids_to_tokens(query_token_id))
+                print(query_token_id)
+                print(tokenizer.convert_ids_to_tokens([id_tensor.data[0] for id_tensor in query_token_id]))
                 input("AAA")
 
             query_output_tensor, fact_output_tensor = self.forward_train(batch["query_token_ids"].to(self.device),
